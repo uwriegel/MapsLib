a520bd9d4dcc53af73da78416c055367b5d5bf4e
Uwe Riegel <uwriegel@gmail.com>
2018-04-09 19:44:08 +0200

    Erfolgreich mit Xamarin und ViewGroup  und References eingebunden

--- app/src/main/java/eu/selfhost/riegel/mapslib/MapsView.java
+++ app/src/main/java/eu/selfhost/riegel/mapslib/MapsView.java
@@ -1,7 +1,37 @@
 package eu.selfhost.riegel.mapslib;
 
-public class MapsView {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+import org.mapsforge.core.model.BoundingBox;
+import org.mapsforge.core.model.Dimension;
+import org.mapsforge.core.model.LatLong;
+import org.mapsforge.map.layer.Layer;
+import org.mapsforge.map.layer.LayerManager;
+import org.mapsforge.map.model.Model;
+import org.mapsforge.map.scalebar.MapScaleBar;
+import org.mapsforge.map.util.MapViewProjection;
+import org.mapsforge.map.view.FpsCounter;
+import org.mapsforge.map.view.FrameBuffer;
+import org.mapsforge.map.view.MapView;
+
+public class MapsView extends ViewGroup  {
+
+    public MapsView(Context context) {
+        super(context);
+    }
+
+    public MapsView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
     public String test(String text) {
-        return "Das wärs: " + text;
+        return "Das wärs mit MapView: " + text;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
     }
 }
