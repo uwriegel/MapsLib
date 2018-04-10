f38926c142c7c7bf8fb21cb0eef4b03a8db20958
Uwe Riegel <uwriegel@gmail.com>
2018-04-10 19:24:53 +0200

    MapsView fertig

--- app/src/main/java/eu/selfhost/riegel/mapslib/MapsView.java
+++ app/src/main/java/eu/selfhost/riegel/mapslib/MapsView.java
@@ -1,18 +1,25 @@
 package eu.selfhost.riegel.mapslib;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.os.Handler;
+import android.os.Looper;
 import android.text.Layout;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
 import android.view.ViewDebug;
 import android.view.ViewGroup;
 
+import org.mapsforge.core.graphics.GraphicContext;
 import org.mapsforge.core.model.BoundingBox;
 import org.mapsforge.core.model.Dimension;
 import org.mapsforge.core.model.LatLong;
+import org.mapsforge.core.model.Point;
 import org.mapsforge.core.util.Parameters;
 import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
-import org.mapsforge.map.android.input.TouchGestureHandler;
 import org.mapsforge.map.controller.FrameBufferController;
 import org.mapsforge.map.controller.LayerManagerController;
 import org.mapsforge.map.controller.MapViewController;
@@ -22,6 +29,7 @@ import org.mapsforge.map.layer.Layers;
 import org.mapsforge.map.layer.TileLayer;
 import org.mapsforge.map.layer.renderer.TileRendererLayer;
 import org.mapsforge.map.model.Model;
+import org.mapsforge.map.model.common.Observer;
 import org.mapsforge.map.scalebar.DefaultMapScaleBar;
 import org.mapsforge.map.scalebar.MapScaleBar;
 import org.mapsforge.map.util.MapPositionUtil;
@@ -32,19 +40,16 @@ import org.mapsforge.map.view.FrameBufferHA;
 import org.mapsforge.map.view.FrameBufferHA2;
 import org.mapsforge.map.view.MapView;
 
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
-
-class MapsView extends ViewGroup implements MapView {
+class MapsView extends ViewGroup implements MapView, Observer {
 
     public MapsView(Context context) {
         super(context);
-        init();
+        init(context);
     }
 
     public MapsView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(context);
     }
 
     public void onCenter() {
@@ -65,8 +70,65 @@ class MapsView extends ViewGroup implements MapView {
     }
 
     @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        // Child views (besides zoom controls)
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            if (child.getVisibility() != View.GONE && checkLayoutParams(child.getLayoutParams())) {
+                MapsView.LayoutParams params = (MapsView.LayoutParams) child.getLayoutParams();
+                int childWidth = child.getMeasuredWidth();
+                int childHeight = child.getMeasuredHeight();
+                Point point = getMapViewProjection().toPixels(params.latLong);
+                if (point != null) {
+                    int childLeft = getPaddingLeft() + (int)Math.round(point.x);
+                    int childTop = getPaddingTop() + (int)Math.round(point.y);
+                    switch (params.alignment) {
+                        case TOP_LEFT:
+                            break;
+                        case TOP_CENTER:
+                            childLeft -= childWidth / 2;
+                            break;
+                        case TOP_RIGHT:
+                            childLeft -= childWidth;
+                            break;
+                        case CENTER_LEFT:
+                            childTop -= childHeight / 2;
+                            break;
+                        case CENTER:
+                            childLeft -= childWidth / 2;
+                            childTop -= childHeight / 2;
+                            break;
+                        case CENTER_RIGHT:
+                            childLeft -= childWidth;
+                            childTop -= childHeight / 2;
+                            break;
+                        case BOTTOM_LEFT:
+                            childTop -= childHeight;
+                            break;
+                        case BOTTOM_CENTER:
+                            childLeft -= childWidth / 2;
+                            childTop -= childHeight;
+                            break;
+                        case BOTTOM_RIGHT:
+                            childLeft -= childWidth;
+                            childTop -= childHeight;
+                            break;
+                    }
+                    child.layout(childLeft, childTop, childLeft + childWidth, childTop + childHeight);
+                }
+            }
+        }
+    }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onSizeChanged(int width, int height, int oldw, int oldh) {
+        this.model.mapViewDimension.setDimension(new Dimension(width, height));
     }
 
     @Override
@@ -76,11 +138,21 @@ class MapsView extends ViewGroup implements MapView {
 
     @Override
     public void destroy() {
+        touchGestureHandler.destroy();
+        layoutHandler.removeCallbacksAndMessages(null);
         layerManager.finish();
         layerManager = null;
+        frameBufferController.destroy();
+        frameBuffer.destroy();
+        if (mapScaleBar != null)
+            mapScaleBar.destroy();
         model.mapViewPosition.destroy();
     }
 
+    /**
+     * Clear all map view elements.<br></br>
+     * i.e. layers, tile cache, label store, map view, resources, etc.
+     */
     @Override
     public void destroyAll() {
         Layers layers = layerManager.getLayers();
@@ -94,6 +166,22 @@ class MapsView extends ViewGroup implements MapView {
                 ((TileRendererLayer)layer).getLabelStore().clear();
         }
         destroy();
+    }
+
+    @Override
+    public ViewGroup.LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new LayoutParams(getContext(), attrs);
+    }
+
+    @Override
+    protected ViewGroup.LayoutParams generateLayoutParams(ViewGroup.LayoutParams p) {
+        return new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, null, Alignment.BOTTOM_CENTER);
+    }
+
+    @Override
+    protected ViewGroup.LayoutParams generateDefaultLayoutParams() {
+        return new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT,
+                null, Alignment.BOTTOM_CENTER);
     }
 
     @Override
@@ -118,17 +206,17 @@ class MapsView extends ViewGroup implements MapView {
 
     @Override
     public LayerManager getLayerManager() {
-        return null;
+        return layerManager;
     }
 
     @Override
     public MapScaleBar getMapScaleBar() {
-        return null;
+        return mapScaleBar;
     }
 
     @Override
     public MapViewProjection getMapViewProjection() {
-        return null;
+        return mapViewProjection;
     }
 
     @Override
@@ -138,56 +226,90 @@ class MapsView extends ViewGroup implements MapView {
 
     @Override
     public void repaint() {
-
+        if (Thread.currentThread().equals(Looper.getMainLooper().getThread()))
+            invalidate();
+        else
+            postInvalidate();
     }
 
     @Override
     public void setCenter(LatLong center) {
-
+        model.mapViewPosition.setCenter(center);
     }
 
     @Override
     public void setMapScaleBar(MapScaleBar mapScaleBar) {
-
+        if (this.mapScaleBar != null)
+            this.mapScaleBar.destroy();
+        this.mapScaleBar = mapScaleBar;
     }
 
     @Override
     public void setZoomLevel(byte zoomLevel) {
-
+        model.mapViewPosition.setZoomLevel(zoomLevel);
     }
 
     @Override
     public void setZoomLevelMax(byte zoomLevelMax) {
-
+        model.mapViewPosition.setZoomLevelMax(zoomLevelMax);
     }
 
     @Override
     public void setZoomLevelMin(byte zoomLevelMin) {
+        model.mapViewPosition.setZoomLevelMin(zoomLevelMin);
+    }
 
+    @Override
+    public void onChange() {
+        // Request layout for child views (besides zoom controls)
+        for (int i = 0; i < getChildCount(); i++) {
+            layoutHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    requestLayout();
+                }
+            });
+            break;
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas androidCanvas) {
+        org.mapsforge.core.graphics.Canvas graphicContext = AndroidGraphicFactory.createGraphicContext(androidCanvas);
+        frameBuffer.draw(graphicContext);
+        if (mapScaleBar != null) {
+            mapScaleBar.draw(graphicContext);
+        }
+        fpsCounter.draw(graphicContext);
+        graphicContext.destroy();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (!isClickable())
+            return false;
+
+        if (gestureDetectorExternal != null && gestureDetectorExternal.onTouchEvent(event))
+            return true;
+
+        boolean retVal = scaleGestureDetector.onTouchEvent(event);
+        if (!scaleGestureDetector.isInProgress())
+            retVal = this.gestureDetector.onTouchEvent(event);
+
+        return retVal;
     }
 
     public void setLocationSetter(LocationSetter setter) {
         this.setter = setter;
     }
 
-    void init() {
-        layoutHandler = new Handler() {
-            @Override
-            public void publish(LogRecord record) {
+    public void setGestureDetector(GestureDetector gestureDetector) {
+        gestureDetectorExternal = gestureDetector;
+    }
 
-            }
-
-            @Override
-            public void flush() {
-
-            }
-
-            @Override
-            public void close() throws SecurityException {
-
-            }
-        };
-        //descendantFocusability = ViewGroup.FOCUS_BLOCK_DESCENDANTS
+    void init(Context context) {
+        layoutHandler = new Handler();
+        setDescendantFocusability(ViewGroup.FOCUS_BLOCK_DESCENDANTS);
         setWillNotDraw(false);
 
         model = new Model();
@@ -201,19 +323,19 @@ class MapsView extends ViewGroup implements MapView {
 
         layerManager = new LayerManager(this, model.mapViewPosition, GRAPHIC_FACTORY);
         layerManager.start();
-        LayerManagerController.create(this.layerManager, model);
+        LayerManagerController.create(layerManager, model);
 
         MapViewController.create(this, model);
 
-        //touchGestureHandler = new TouchGestureHandler(this);
-        //this.gestureDetector = GestureDetector(context, touchGestureHandler)
-        //this.scaleGestureDetector = ScaleGestureDetector(context, touchGestureHandler)
+        touchGestureHandler = new TouchGestureHandler(this);
+        gestureDetector = new GestureDetector(context, touchGestureHandler);
+        scaleGestureDetector = new ScaleGestureDetector(context, touchGestureHandler);
 
-        this.mapScaleBar = new DefaultMapScaleBar(model.mapViewPosition, model.mapViewDimension,
+        mapScaleBar = new DefaultMapScaleBar(model.mapViewPosition, model.mapViewDimension,
                 GRAPHIC_FACTORY, model.displayModel);
         mapViewProjection = new MapViewProjection(this);
 
-        //model.mapViewPosition.addObserver(this);
+        model.mapViewPosition.addObserver(this);
     }
 
     enum Alignment {
@@ -261,4 +383,5 @@ class MapsView extends ViewGroup implements MapView {
     private MapViewProjection mapViewProjection;
     private TouchGestureHandler touchGestureHandler;
     private LocationSetter setter;
+    private ScaleGestureDetector scaleGestureDetector;
 }
